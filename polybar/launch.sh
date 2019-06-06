3f72c900dbbf9658141ee75c31883b277fbb572e
Ress <git@x39.dev>
2019-06-06 15:09:27 +0900

    linux: polybar update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar bar0 &
+polybar ${1:-"bar0"} &
 
 echo "Bars launched..."
