909066e14fabc968a579ba374148befc077f4a3a
Ress <git@x39.dev>
2020-04-21 13:06:48 +0900

    mac と linux 用のデータを分離

---
+++ etc/init/brew.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+brew insatll fd
+brew install fzf
+brew install fzy
+brew install ghq
+brew install go
+brew install hugo
+brew install jq
+brew install ripgrep
+brew install tig
+brew install tree
